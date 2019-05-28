f87cb3e973fd84ea96a6dcbb7774a75443e72b59
Philip Johnson <phil7j@gmail.com>
2019-05-28 15:31:07 -0700

    complete mvp with minimum styling

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay(props){
+   return (
+   <div className={props.displayStyle}>
+        <h1 className={props.h1Style}>{props.text}</h1>
+    </div>
+   )
+}
+
+export default CalculatorDisplay
