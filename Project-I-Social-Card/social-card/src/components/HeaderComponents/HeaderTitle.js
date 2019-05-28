f4637aac461670028cb56a7ecd0c545aa0f81083
Philip Johnson <phil7j@gmail.com>
2019-05-27 17:13:25 -0700

    complete mvp for first project

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(){
+    return (
+        <div className="header-title">
+            <h1>Lambda School</h1>
+            <h2>@LambdaSchool</h2>
+            <p>27 May</p>
+        </div>
+    )
+}
+
+export default HeaderTitle
